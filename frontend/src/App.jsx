333cb2044420132f712a77c0794fedccc12a2597
unknown <ramkiran6936@gmail.com>
2026-09-23 10:49:37 +0530

    New Routing in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home.jsx'
+import Login from './Pages/Login.jsx'
+import About from './Pages/About.jsx'
+import Registeration from './Pages/Registeration.jsx'
 export default function App() {
   return( 
-  <div>
-    <h2>👋Welcome to E Info</h2>    
-  </div>
+  <Router>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Registeration />} />
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </Router>
   );
 }
